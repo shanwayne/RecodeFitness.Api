b1f55e7a1339c42c95727c599fc3a7d9e4c5979e
Almando <almando.c.mason@gmail.com>
2021-10-21 22:15:36 -0400

    Add project files.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecodeFitness.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public Profile GetProfile()
+        {
+            return new Profile { Name = "Almando Mason" };
+        }
+    }
+
+    public class Profile
+    {
+        public string Name {  get; set; }        
+    }
+}
